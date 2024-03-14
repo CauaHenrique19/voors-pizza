3984f454113b1071eed58a1aa2083c73321e766a
CauaHenrique19 <cauah6002@gmail.com>
2024-03-14 09:14:13 -0300

    feat: add not found http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,10 +1,15 @@
-import { ServerError } from '../errors';
-import { HttpResponse } from '../protocols';
+import { ServerError } from 'src/presentation/errors';
+import { HttpResponse } from 'src/presentation/protocols';
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error.message,
   name: error?.name,
+});
+
+export const notFound = (): HttpResponse => ({
+  body: null,
+  statusCode: 404,
 });
 
 export const serverError = (error: Error): HttpResponse => ({
