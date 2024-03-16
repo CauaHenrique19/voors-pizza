2877d0b42a61f5eeb8382b9903e1ff11c57f7d1a
CauaHenrique19 <cauah6002@gmail.com>
2024-03-16 03:32:40 -0300

    feat: throw error if not have value

--- src/data/usecases/size/find-sizes.ts
+++ src/data/usecases/size/find-sizes.ts
@@ -1,10 +1,17 @@
 import { FindSizesRepository } from 'src/data/protocols/db';
+import { SizesNotFoundError } from 'src/domain/errors';
 import { FindSizesUseCase } from 'src/domain/usecases';
 
 export class FindSizes implements FindSizesUseCase {
   constructor(private readonly findSizesRepository: FindSizesRepository) {}
 
   async find(): Promise<FindSizesUseCase.Result> {
-    return await this.findSizesRepository.find();
+    const sizes = await this.findSizesRepository.find();
+
+    if (!sizes.length) {
+      throw new SizesNotFoundError();
+    }
+
+    return sizes;
   }
 }
