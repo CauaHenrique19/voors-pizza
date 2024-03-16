364e781fe2c8e6b67dd8f149703ce4bfa1ba356b
CauaHenrique19 <cauah6002@gmail.com>
2024-03-15 21:42:12 -0300

    feat: implement factory to build controller of create order usecase

---
+++ src/main/factories/controllers/order/create-order-controller.factory.ts
@@ -0,0 +1,16 @@
+import { Inject } from '@nestjs/common';
+import { CREATE_ORDER_FACTORY } from 'src/main/factories/providers';
+import { CreateOrderUseCase } from 'src/domain/usecases/order';
+import { Controller } from 'src/presentation/protocols';
+import { CreateOrderController } from 'src/presentation/controllers';
+
+export class BuildCreateOrderControllerFactory {
+  constructor(
+    @Inject(CREATE_ORDER_FACTORY)
+    private readonly createOrderUseCase: CreateOrderUseCase,
+  ) {}
+
+  public build(): Controller {
+    return new CreateOrderController(this.createOrderUseCase);
+  }
+}
