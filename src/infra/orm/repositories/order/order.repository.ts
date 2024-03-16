5a30ca6bd51c3053624a87c9bf1e54bbeb90e1fd
CauaHenrique19 <cauah6002@gmail.com>
2024-03-16 01:10:23 -0300

    feat: implement find method

--- src/infra/orm/repositories/order/order.repository.ts
+++ src/infra/orm/repositories/order/order.repository.ts
@@ -1,7 +1,12 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import { CreateOrderRepository } from 'src/data/protocols/db';
+import {
+  CreateOrderRepository,
+  FindOrderByIdRepository,
+} from 'src/data/protocols/db';
 
-export class OrderPrismaRepository implements CreateOrderRepository {
+export class OrderPrismaRepository
+  implements CreateOrderRepository, FindOrderByIdRepository
+{
   private orderRepository: Prisma.OrderDelegate;
 
   constructor() {
@@ -15,5 +20,62 @@ export class OrderPrismaRepository implements CreateOrderRepository {
   ): Promise<CreateOrderRepository.Result> {
     const client = transaction ? transaction.order : this.orderRepository;
     return await client.create({ data: parameters });
+  }
+
+  async find(
+    parameters: FindOrderByIdRepository.Parameters,
+  ): Promise<FindOrderByIdRepository.Result> {
+    return await this.orderRepository.findFirst({
+      where: { id: parameters.id },
+      select: {
+        id: true,
+        total: true,
+        totalPreparationTime: true,
+        createdAt: true,
+        itens: {
+          select: {
+            id: true,
+            orderId: true,
+            sizeId: true,
+            flavourId: true,
+            createdAt: true,
+            size: {
+              select: {
+                id: true,
+                description: true,
+                preparationTime: true,
+                value: true,
+                createdAt: true,
+              },
+            },
+            flavour: {
+              select: {
+                id: true,
+                description: true,
+                adittionalPreparationTime: true,
+                createdAt: true,
+              },
+            },
+            personalizations: {
+              select: {
+                id: true,
+                createdAt: true,
+                orderItemId: true,
+                personalizationId: true,
+                personalization: {
+                  select: {
+                    id: true,
+                    description: true,
+                    value: true,
+                    adittionalPreparationTime: true,
+                    createdAt: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
   }
 }
