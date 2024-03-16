8ceb79407ab5988d345e3514a19085766cb2dcba
CauaHenrique19 <cauah6002@gmail.com>
2024-03-15 21:05:49 -0300

    feat: add usecase of create order

---
+++ src/domain/usecases/order/create-order.ts
@@ -0,0 +1,17 @@
+import { OrderItemModel, OrderModel } from 'src/domain/models';
+
+export interface CreateOrderUseCase {
+  create(
+    parameters: CreateOrderUseCase.Parameters,
+  ): Promise<CreateOrderUseCase.Result>;
+}
+
+export namespace CreateOrderUseCase {
+  export type Parameters = {
+    items: (Pick<OrderItemModel, 'sizeId' | 'flavourId'> & {
+      personalizations?: number[];
+    })[];
+  };
+
+  export type Result = OrderModel;
+}
