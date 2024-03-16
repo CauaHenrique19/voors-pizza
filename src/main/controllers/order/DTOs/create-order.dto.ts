e9bac690b0de2f9e035579325bd8cb3605d8a20a
CauaHenrique19 <cauah6002@gmail.com>
2024-03-15 21:42:46 -0300

    feat: implement dto of create order

---
+++ src/main/controllers/order/DTOs/create-order.dto.ts
@@ -0,0 +1,36 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  ArrayMinSize,
+  IsNumber,
+  IsOptional,
+  ValidateNested,
+  IsInt,
+} from 'class-validator';
+
+export class CreateOrderDTO {
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDTO)
+  items: OrderItemDTO[];
+}
+
+class OrderItemDTO {
+  @IsNumber({}, { message: 'sizeId deve ser um numero' })
+  @IsInt({ message: 'sizeId deve ser um numero inteiro' })
+  sizeId: number;
+
+  @IsNumber({}, { message: 'flavourId deve ser um numero' })
+  @IsInt({ message: 'flavourId deve ser um numero inteiro' })
+  flavourId: number;
+
+  @IsOptional()
+  @IsArray({ message: 'personalizations deve ser um array' })
+  @ArrayMinSize(1, {
+    message: 'personalizations deve conter ao menos 1 item',
+  })
+  @IsNumber({}, { each: true, message: 'personalization deve ser um numero' })
+  @IsInt({ each: true, message: 'personalization deve ser um numero inteiro' })
+  personalizations: number[];
+}
