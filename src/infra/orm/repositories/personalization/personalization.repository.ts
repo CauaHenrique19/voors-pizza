9b857f3cea2e77bb45b7bf9c077a53bba7af6d6d
CauaHenrique19 <cauah6002@gmail.com>
2024-03-15 21:20:22 -0300

    feat: implement find personalization by id

--- src/infra/orm/repositories/personalization/personalization.repository.ts
+++ src/infra/orm/repositories/personalization/personalization.repository.ts
@@ -1,8 +1,11 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import { FindPersonalizationsRepository } from 'src/data/protocols/db';
+import {
+  FindPersonalizationByIdRepository,
+  FindPersonalizationsRepository,
+} from 'src/data/protocols/db';
 
 export class PersonalizationPrismaRepository
-  implements FindPersonalizationsRepository
+  implements FindPersonalizationsRepository, FindPersonalizationByIdRepository
 {
   private personalizationRepository: Prisma.PersonalizationDelegate;
 
@@ -13,5 +16,19 @@ export class PersonalizationPrismaRepository
 
   find(): Promise<FindPersonalizationsRepository.Result> {
     return this.personalizationRepository.findMany();
+  }
+
+  async findById(
+    parameters: FindPersonalizationByIdRepository.Parameters,
+  ): Promise<FindPersonalizationByIdRepository.Result> {
+    if (Array.isArray(parameters.id)) {
+      return await this.personalizationRepository.findMany({
+        where: { id: { in: parameters.id } },
+      });
+    }
+
+    return await this.personalizationRepository.findFirst({
+      where: { id: parameters.id },
+    });
   }
 }
